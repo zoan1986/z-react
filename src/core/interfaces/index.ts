64d45252245ba4674e52ce2f550d556dd20399c6
zoan <doanhoangphuong@gmail.com>
2023-06-05 13:55:45 +0700

    MVC

---
+++ src/core/interfaces/index.ts
@@ -0,0 +1,3 @@
+import Route from "./route.interface";
+
+export { Route };
---
+++ src/module/index/index.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { Route } from "src/core/interfaces";
+import IndexController from "./index.controller";
+
+export default class IndexRoute implements Route{
+    public path = '/';
+    public router = Router();
+
+    public IndexController = new IndexController();
+    
+    constructor() { this.initializeRoutes(); }
+    
+    private initializeRoutes(){
+        this.router.get(this.path, this.IndexController.index);
+    }
+    
+}
