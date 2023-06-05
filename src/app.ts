9907907125673e2f0a6b4936dc9ba03b30144efb
zoan <doanhoangphuong@gmail.com>
2023-06-05 16:02:29 +0700

    middleware for security cors, helmet, morgan, hpp

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,30 @@
 import { Route } from "./core/interfaces";
 import express from "express";
 import mongoose from "mongoose";
+import hpp from 'hpp';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Logger } from "./core/utils";
 
 class App{
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
 
     constructor(routes: Route[]){
         this.app = express();
         this.port = process.env.PORT || 6000;
+        this.production = process.env.NODE_ENV =='production' ?true : false;
 
         this.initializeRoutes(routes);
         this.connectToDatabase();
+        this.initializeMiddleware();
     }
 
     public listen(){
         this.app.listen(this.port, () => { 
-            console.log(`Server is listening on port ${this.port}`)
+            Logger.info(`Server is listening on port ${this.port}`)
         });
     }
     
@@ -26,18 +34,30 @@ class App{
         });
     }
 
+    private initializeMiddleware(){
+        if (!this.production){
+            this.app.use(hpp());
+            this.app.use(helmet()); 
+            this.app.use(morgan("combined"));
+            this.app.use(
+                cors({origin: 'link web', credentials: true})
+            );
+        }else {
+            this.app.use(morgan('dev'));
+            this.app.use(cors({origin: true, credentials: true }));
+        }
+    }
+
     private connectToDatabase(){ 
-        try {
             const connectString = process.env.MONGODB_URI;
             if(!connectString){
-                console.log('Connection string is invalid');
+                Logger.error('Connection string is invalid');
                 return;
             }
-            mongoose.connect(connectString);
-            console.log('DB connected');
-        } catch (error){
-            console.log('Connect BD error');
-        }
+            mongoose.connect(connectString).catch((reason)=>{
+                Logger.error(reason)
+            });
+            Logger.info('DB connected')
     }
 
 }
